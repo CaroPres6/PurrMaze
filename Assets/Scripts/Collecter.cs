c40a475cf4eea841acd36a8e4f7346f2eb56f98b
CaroPres6 <2043572@cegepmv.ca>
2026-09-24 17:49:40 -0400

    Ajout lait à collecter

---
+++ Assets/Scripts/Collecter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Collecter : MonoBehaviour
+{
+    [SerializeField, Min(1)] private int valeur = 1;
+    private bool collectee;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collectee || !collision.CompareTag("Player")) return;
+        collectee = true;
+        GestionJeu.Instance.AjouterLait(valeur);
+        Destroy(gameObject);
+    }
+}
